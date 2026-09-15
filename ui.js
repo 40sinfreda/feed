ec02828734571191840b4c7dc9f6d69728c028b9
40sinfreda <h.israel2008@gmail.com>
2026-09-15 13:55:52 +0300

    Keep Sheets defaults and PWA UI helpers

--- ui.js
+++ ui.js
@@ -112,5 +112,5 @@
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("./sw.js").catch(function () {});
   }
-  startSplash();
+  if (!window.__splashStarted) { window.__splashStarted = true; startSplash(); }
 })();
